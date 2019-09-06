dfbe207d70974315bc66e35977497ae7dee24faa
dung <dung@192.168.14.133>
2019-09-06 11:52:31 -0600

    helpMeStingQuiz

--- StringQuiz/src/StringQuiz.java
+++ StringQuiz/src/StringQuiz.java
@@ -6,7 +6,7 @@ public class StringQuiz
 		public static void main(String[] args)
 			{
 				// TODO Auto-generated method stub
-
+				// coding is cool, help im struggling
 				Scanner stringInput = new Scanner (System.in);
 				
 				System.out.println("Pick two words please");
